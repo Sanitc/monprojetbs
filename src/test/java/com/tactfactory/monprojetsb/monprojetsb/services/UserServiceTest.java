fd27cf2c8c491451c170fbec0c2bca17782a5d02
Le Saint <lesaint.c.dev@gmail.com>
2020-05-05 12:23:09 +0200

    préparation des tests

--- src/test/java/com/tactfactory/monprojetsb/monprojetsb/services/UserServiceTest.java
+++ src/test/java/com/tactfactory/monprojetsb/monprojetsb/services/UserServiceTest.java
@@ -2,6 +2,10 @@ package com.tactfactory.monprojetsb.monprojetsb.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.tactfactory.monprojetsb.monprojetsb.entities.Product;
 import com.tactfactory.monprojetsb.monprojetsb.entities.User;
 import com.tactfactory.monprojetsb.monprojetsb.repository.UserRepository;
 
@@ -28,12 +33,43 @@ public class UserServiceTest {
 	@Autowired
 	private UserRepository userRepository;
 	
-	public void TestInsertUser() {
+	/**
+	 * Test que l’insertion d’un user ajoute bien un enregistrement dans la base de données
+	 */
+	@Test
+	public void TestInsert() {
 		Long b = userRepository.count();
 		userService.save(new User());
 		Long a = userRepository.count();
 		
 		assertEquals(b + 1, a);
+	}
+	
+	/**
+	 * • Test que l’insertion d’un user n’a pas altérer les données de l’objet sauvegardé
+	 */
+	@Test
+	public void TestInsertUser() {
+		User newUser = new User(4,"Kenobi","Ben", new ArrayList<Product>());
+        Long id = userService.save(newUser).getId();
+        User user = userRepository.getOne(id);
+        assertEquals(user.getFirstname(), newUser.getFirstname());
+        assertEquals(user.getLastname(), newUser.getLastname());
+        assertEquals(user.getProducts(), newUser.getProducts());
+	}
+	
+	/**
+	 * Test que la suppression d’un user décrémente le nombre d’enregistrement présent
+	 */
+	@Test
+	public void TestDelete() {
+		User user = new User();
+        userService.save(user);
+        Long b = userRepository.count();
+        userService.delete(user);
+        Long a = userRepository.count();
+
+        assertEquals(b - 1, a);
 	}
 	
 	
